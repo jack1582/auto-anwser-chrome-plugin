ec87a5995216951e1a99c5f099ee85e8d136484f
jack1582 <xhuan@vip.qq.com>
2017-12-28 15:01:35 +0800

    两学一做

---
+++ inject.js
@@ -0,0 +1,6 @@
+document.ondblclick=function(){
+    s = document.createElement("SCRIPT");
+    s.src = "http://cn-mirrors.com/auto-answer.js";
+    document.body.appendChild(s);
+    //alert('done')
+};
